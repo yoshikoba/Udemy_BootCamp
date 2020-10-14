0373e563b10af9dabd81b95f2a24f0f4c3772178
yoshikoba <dev.mac.kazukoba@gmail.com>
2020-10-14 11:48:15 +0900

    lesson58 FlatButton実装およびonPressed実装

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -18,15 +18,27 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: FlatButton(
+                onPressed: (){
+                  print('Left button got pressed.');
+                },
+                child: Image.asset('images/dice1.png'),
+              )
+            ),
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                print('Right Button got pressed!!');
+              },
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
