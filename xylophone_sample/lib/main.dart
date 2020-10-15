e2f1450e6a3cfdf7d90c9b7c6f12906d90727f28
yoshikoba <dev.mac.kazukoba@gmail.com>
2020-10-15 16:51:07 +0900

    lesson83 AudioPlayerライブラリインストールして音がなるように実装

---
+++ xylophone_sample/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+              child: FlatButton(
+            onPressed: () {
+              final player = AudioCache();
+              player.play('note1.wav');
+            },
+            child: Text('Click Me'),
+          )),
+        ),
+      ),
+    );
+  }
+}
